897dba16a2959f9f855660d22b05406ae7efd0a0
Sneanken <924393527@qq.com>
2020-08-01 23:13:33 +0800

    第三章demo

--- src/main.js
+++ src/main.js
@@ -2,11 +2,15 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import { fetchListData } from "@/api/api";
 
 Vue.config.productionTip = false;
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount("#app");
+fetchListData("top").then(items => {
+  window.items = items;
+  new Vue({
+    router,
+    store,
+    render: h => h(App)
+  }).$mount("#app");
+});
---
+++ src/views/ItemList.vue
@@ -0,0 +1,13 @@
+<template>
+  <item />
+</template>
+
+<script>
+import Item from "@/components/Item.vue";
+
+export default {
+  components: {
+    Item
+  }
+};
+</script>
